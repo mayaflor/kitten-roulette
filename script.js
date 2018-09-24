adb7f415a291bfa3443ee77f8d1bb6fab88d1720
Maya Flor <maya.r.rebelo@gmail.com>
2018-09-24 16:21:29 -0300

    able to get random picture from api

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-// $(document).ready(() => {
+// Here's your API key
 
-//   const url = "https://cataas.com/cat"
+// 460e3088-2a9c-452d-a088-ccbfd2686a96
 
-//   $.ajax({
-//     type: "GET",
-//     url,
-//     success: loadCats,
-//     error,
-//   })
+// Here's your user_id
+
+// uf31v0
 
 
-// })
- console.log("jfjd")
-// function loadCats(data){
-//   console.log(data);
+$(document).ready(() => {
 
-// }
+  const url = "https://api.thecatapi.com/v1/images/search?460e3088-2a9c-452d-a088-ccbfd2686a96"
+
+  $.ajax({
+    type: "GET",
+    url,
+    success: loadCats,
+    // error,
+  })
+
+
+})
+
+// data[0]["url"]
+
+function loadCats(data){
+  console.log(data);
+
+  let kittyContainer = `<div class="kitty-container"><img src="${data[0]["url"]}"></img>></div>`
+
+  $("#kitty-area").append(kittyContainer);
+
+}
