f7cc2c86195a04f00bc5cd8131c299821fb36cab
Maya Flor <maya.r.rebelo@gmail.com>
2018-09-24 15:59:09 -0300

    test commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// $(document).ready(() => {
+
+//   const url = "https://cataas.com/cat"
+
+//   $.ajax({
+//     type: "GET",
+//     url,
+//     success: loadCats,
+//     error,
+//   })
+
+
+// })
+ console.log("jfjd")
+// function loadCats(data){
+//   console.log(data);
+
+// }
