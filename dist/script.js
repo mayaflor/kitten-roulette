0cd5643a1039600ff621de4124a65eaed2eac7fa
Maya Flor <maya.r.rebelo@gmail.com>
2018-09-27 17:44:20 -0300

    saving favorites on local storage

--- dist/script.js
+++ dist/script.js
@@ -6,8 +6,6 @@
 
 // uf31v0
 
-
-
 $(document).ready(() => {
 
   const url = "https://api.thecatapi.com/v1/images/search?460e3088-2a9c-452d-a088-ccbfd2686a96" 
@@ -26,19 +24,28 @@ $(document).ready(() => {
     }
  }
 
-
   function getCats(data){
     console.log(data);
   
-    let kittyContainer = `<div class="kitty-container"><img src="${data[0]["url"]}"></div>`
+    let kittyContainer = `<div class="kitty-container" id="parent-${data[0]["id"]}"><img src="${data[0]["url"]}"><i id="${data[0]["id"]}" onclick="clickLike()" class="icon-heart like"></i></div>`
   
     $("#kitty-area").append(kittyContainer);
   }
- 
-  
+
   function favs(){
     console.log("favs")
-    
   }
+
 })
+
+function clickLike(){
+  let catId = $(".like").attr("id")
+  localStorage.setItem("id", catId)
+}
+
+// localStorage.setItem("id", catId)
+
+// localStorage.getItem("id")
+
+// localStorage.removeItem("id")
 
