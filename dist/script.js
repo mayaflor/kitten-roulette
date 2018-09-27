ac6803264082db9d5166fcedf791ff69af30a5fb
Maya Flor <maya.r.rebelo@gmail.com>
2018-09-27 15:20:21 -0300

    routes working

--- dist/script.js
+++ dist/script.js
@@ -5,31 +5,40 @@
 // Here's your user_id
 
 // uf31v0
-page("/", loadCats);
-  page("favs", favs)
+
 
 
 $(document).ready(() => {
+
+  const url = "https://api.thecatapi.com/v1/images/search?460e3088-2a9c-452d-a088-ccbfd2686a96" 
+
+  page("/", one);
+  page("/favs", favs);
+  page();
+  
+ function one(){
+    for(let i=0; i < 4; i++){
+      $.ajax({
+        type: "GET",
+        url,
+        success: getCats
+      })
+    }
+ }
+
+
+  function getCats(data){
+    console.log(data);
+  
+    let kittyContainer = `<div class="kitty-container"><img src="${data[0]["url"]}"></div>`
+  
+    $("#kitty-area").append(kittyContainer);
+  }
  
-  const url = "https://api.thecatapi.com/v1/images/search?460e3088-2a9c-452d-a088-ccbfd2686a96"
-  for(let i=0; i < 4; i++){
-    $.ajax({
-      type: "GET",
-      url,
-      success: loadCats,
-    })
+  
+  function favs(){
+    console.log("favs")
+    
   }
 })
 
-function loadCats(data){
-  console.log(data);
-
-  let kittyContainer = `<div class="kitty-container"><img src="${data[0]["url"]}"></div>`
-
-  $("#kitty-area").append(kittyContainer);
-
-}
-
-function favs(){
-  console.log("favs")
-}
